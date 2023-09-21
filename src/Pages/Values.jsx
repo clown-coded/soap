061ed1fbbf4918631c55b36e9e6e7d85087a643e
Sam Mitchell <samantha.mitchell.uni@gmail.com>
2023-09-21 15:05:48 +1200

    css fixes

--- src/Pages/Values.jsx
+++ src/Pages/Values.jsx
@@ -6,30 +6,59 @@ const Values = () => {
   return (
     <div className='values-wrapper'>
 
-      <img src={bg} className='home-bg' alt="" />
-      <img src={bg} className='home-bg reverse' alt="" />
-        <div className="charter-container">
-         
-        
+      {/* <img src={bg} className='home-bg' alt="" />
+      <img src={bg} className='home-bg reverse' alt="" /> */}
+      <div className="values-container">
+        <h2>Our Values</h2>
+        <h4>The work carried out by our movement and political party is guided by a strong sense of purpose, rooted in our charter and the core values that unite us. These values are not just words on paper; they are the principles that drive our actions and shape our vision for New Zealand. Collectively, we are committed to:</h4>
+        <p className='values-heading'>Acting according to our charter:</p>
+        <p> Our charter serves as the compass that guides our decisions and actions. It embodies our shared vision for a better New Zealand and outlines the principles we hold dear.</p>
+        <p className='values-heading'>Making New Zealand a place we can all be proud to call home: </p>
+        <p> We aspire to create a nation where every resident, regardless of their background, feels a deep sense of pride and belonging. We work tirelessly to foster an inclusive and welcoming society.</p>
+        <p className='values-heading'>Offering a better solution to the problem:</p>
+        <p>We are not content with the status quo. We continually seek innovative and effective solutions to the challenges facing our nation, striving to improve the lives of all New Zealanders.
+</p>
+        <p className='values-heading'>Believing in an individual's freedom: </p>
+        <p>We firmly believe in the importance of individual liberties and freedoms. Our policies and actions are guided by the principle that every person should have the freedom to make choices that are best for them within the bounds of a just and equitable society.
+</p>
+        <p className='values-heading'>Committing to hard work: </p>
+        <p>We understand that achieving meaningful change and progress requires dedication and effort. We are committed to rolling up our sleeves and working tirelessly to bring about positive outcomes for our country.
+</p>
+<p className='values-heading'>Acting with kindness: </p>
+        <p>Kindness is at the heart of our approach to governance and community. We believe in the power of compassion, empathy, and cooperation to create a more harmonious and caring New Zealand.</p>
+<p className='values-heading'>Upholding honesty within the Government:</p>
+        <p> We hold ourselves to the highest standards of transparency, accountability, and integrity. We are dedicated to ensuring that the government operates with honesty and serves the best interests of the people.</p>
+<p className='values-heading'>Creating a more cohesive New Zealand: </p>
+        <p> We recognize the strength that comes from unity and cohesion. We work towards bridging divides, promoting understanding, and building a more connected and harmonious society.</p>
+<h4>In all our endeavors, we remain steadfast in our commitment to these values, which serve as the foundation upon which we strive to build a better, more prosperous, and equitable New Zealand for everyone. Together, as a movement and political party, we are dedicated to the pursuit of these ideals, making New Zealand a beacon of hope and progress.</h4>
 
+        </div>
+
+
+
+        <div className="charter-container">
         <h2>Our Charter</h2>
         <h4>This document serves as the foundational charter of The SOAP Party of New Zealand, encapsulating our core principles and values </h4>
-          <p> Striving for What's Right and Honest:<br/>
+          <p className='values-heading'> Striving for What's Right and Honest:</p>
+        <p>
 We firmly believe that honesty is the linchpin of effective policy-making. As a party, our commitment to transparency and truthfulness underpins our mission to serve the people of New Zealand to the best of our abilities.</p>
          
-          <p>Conducting Ourselves with Poise and Professionalism: <br/>
+          <p className='values-heading'>Conducting Ourselves with Poise and Professionalism: </p>
+        <p>
 Members of our party are dedicated to upholding the highest standards of conduct. We pledge to represent our constituents with dignity, professionalism, and an unwavering commitment to our party's fundamental principles.
           </p>
 
-         <p>No Empty Promises: <br/>
+         <p className='values-heading'>No Empty Promises:</p>
+        <p>
 In our pursuit of your support, we make a solemn promise not to make commitments that we cannot or will not fulfill. Our policy-making will be guided by realism and integrity, ensuring that the promises we make to the people of New Zealand are sincere and attainable.
 
 
 </p>
-<p>Progressive Policies with a Focus on Stability: <br/>
+<p className='values-heading'>Progressive Policies with a Focus on Stability: </p>
+        <p>
 Our policy approach is rooted in progressivism while acknowledging the importance of stability. We endeavor to craft policies that resonate with a diverse range of individuals, striking a delicate balance between advancing our society and safeguarding against any regressive impacts.
 </p>
-<p>In embracing these principles, The SOAP Party of New Zealand seeks to chart a path forward that prioritizes the well-being and prosperity of our nation, guided by an unwavering commitment to truth, integrity, and thoughtful policy-making. </p>
+<h4>In embracing these principles, The SOAP Party of New Zealand seeks to chart a path forward that prioritizes the well-being and prosperity of our nation, guided by an unwavering commitment to truth, integrity, and thoughtful policy-making. </h4>
 
 
 
@@ -38,24 +67,7 @@ Our policy approach is rooted in progressivism while acknowledging the importanc
 
         </div>
 
-        <div className="values-container">
-        <h2>Our Values</h2>
-        <h4>The work carried out by our movement and political party is guided by a strong sense of purpose, rooted in our charter and the core values that unite us. These values are not just words on paper; they are the principles that drive our actions and shape our vision for New Zealand. Collectively, we are committed to:</h4>
-        <p>Acting according to our charter:<br/> Our charter serves as the compass that guides our decisions and actions. It embodies our shared vision for a better New Zealand and outlines the principles we hold dear.</p>
-        <p>Making New Zealand a place we can all be proud to call home: <br/> We aspire to create a nation where every resident, regardless of their background, feels a deep sense of pride and belonging. We work tirelessly to foster an inclusive and welcoming society.</p>
-        <p>Offering a better solution to the problem: <br/>We are not content with the status quo. We continually seek innovative and effective solutions to the challenges facing our nation, striving to improve the lives of all New Zealanders.
-</p>
-        <p>Believing in an individual's freedom: <br/>We firmly believe in the importance of individual liberties and freedoms. Our policies and actions are guided by the principle that every person should have the freedom to make choices that are best for them within the bounds of a just and equitable society.
-</p>
-        <p>Committing to hard work: <br/>We understand that achieving meaningful change and progress requires dedication and effort. We are committed to rolling up our sleeves and working tirelessly to bring about positive outcomes for our country.
-</p>
-<p>Acting with kindness: <br/>Kindness is at the heart of our approach to governance and community. We believe in the power of compassion, empathy, and cooperation to create a more harmonious and caring New Zealand.</p>
-<p>Upholding honesty within the Government:<br/> We hold ourselves to the highest standards of transparency, accountability, and integrity. We are dedicated to ensuring that the government operates with honesty and serves the best interests of the people.</p>
-<p>Creating a more cohesive New Zealand: <br/> We recognize the strength that comes from unity and cohesion. We work towards bridging divides, promoting understanding, and building a more connected and harmonious society.</p>
-<p>In all our endeavors, we remain steadfast in our commitment to these values, which serve as the foundation upon which we strive to build a better, more prosperous, and equitable New Zealand for everyone. Together, as a movement and political party, we are dedicated to the pursuit of these ideals, making New Zealand a beacon of hope and progress.</p>
-
-        </div>
-
+       
     </div>
   )
 }
